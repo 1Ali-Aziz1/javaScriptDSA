338f513bf2ab53535d34aa36e5bb3ee72333b510
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-01-16 18:22:35 +0530

    Fixed quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,38 +1,44 @@
-
-
 function quickSort(arr, start, last) {
-    // if(last<=start) return;//Base case
-    
-    if (start < last) {
-        let pivot = partition(arr, start, last);
-        quickSort(arr, start, pivot-1);
-        quickSort(arr, pivot+1, last);
-    }
-    return arr
+  if (start < last) {
+    let pivot = partition(arr, start, last);
+    quickSort(arr, start, pivot - 1);
+    quickSort(arr, pivot + 1, last);
+  }
+  return arr;
 }
 
 function partition(arr, start, last) {
-    let i = start -1
-    console.log(start);
-    let pivot = arr[last];
-    
-    for (let j = start; j <= last; j++) {
-        if (arr[j] < pivot) {
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            console.log("hi");
-            i++;
-        }
+  let i = start - 1;
+  // console.log(start);
+  let pivot = arr[last];
+
+  for (let j = start; j <= last; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      // console.log("hi");
     }
-    i++;
-	temp = arr[i];
-	arr[i] = arr[last];
-	arr[last] = temp;
-    return i;
+  }
+  i++;
+  temp = arr[i];
+  arr[i] = arr[last];
+  arr[last] = temp;
+  return i;
 }
 
-arr = [3, 5, 9, 8, 2, 1]
+function generateRandomNumbers() {
+  let arr = [];
+  for (let i = 0; i < 100; i++) {
+    arr.push(Math.floor(Math.random() * 100) + 1);
+  }
+  return arr;
+}
 
-arr = quickSort(arr, 0, arr.length -1)
-console.log(arr);
+arr = generateRandomNumbers();
+
+console.log("Initial value of the array", arr);
+
+arr = quickSort(arr, 0, arr.length - 1);
+console.log("Sorted array", arr);
