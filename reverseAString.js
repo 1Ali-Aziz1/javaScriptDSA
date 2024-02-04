90884ff758f01d363b353463242d454246b0a941
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-02-04 20:55:31 +0530

    Added some more comments.

--- reverseAString.js
+++ reverseAString.js
@@ -7,15 +7,15 @@
 function reverseString(str) {
     //The parameter "str" is the string we will reverse
     
-    letters = [];//This array stores the string letters
+    letters = [];//This array stores the string letters.
     reversedStr = "";//This variable stores reversed string
 
     for (let i = 0; i < str.length; i++) {
         //This loop puts the string's letters to array(variable letters)
-        letters.push(str[i]);//Pushes i'th letter to the array(variable letters)
+        letters.push(str[i]);//Pushes i'th letter to the array(variable letters).
     }
     for (let i = 0; i < str.length; i++) {
-        //This loop adds letters to the reversed string(variable reversedStr)
+        //This loop adds letters to the reversed string(variable reversedStr).
         temp = reversedStr;//Storing the reversed string's(variable reversedSr) value.
         reversedStr = temp + letters[letters.length-i-1];//Adds the old reversed string(variable reversedStr) and a new letter from the the array(variable reversedStr) but from reverse.
     }
@@ -34,5 +34,5 @@ console.log("Reversed string: "+reversedString);//Shows the reversed array.
 if (string != reversedStr) {
     console.log(string + " isn't a palindrome");//Shows that it's a palindrome.
 } else {
-    console.log(string + " is a palindrome");
+    console.log(string + " is a palindrome");//Shows that it's not a palindrome
 }
