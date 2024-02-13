866d5f9e94cd124b12d97498b9c6e6a464d6988a
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-02-13 19:05:20 +0530

    Just a little more

--- sortStringArrays.js
+++ sortStringArrays.js
@@ -3,6 +3,7 @@
 //Note: This function uses bubbleSort algorithm
 
 function sortStringArrays(arr) {
+    //Main function
     for (let i = 0; i < arr.length-1; i++) {
         for (let j = 0; j < arr.length-i-1; j++) {
             if (arr[j]>arr[j+1]) {
