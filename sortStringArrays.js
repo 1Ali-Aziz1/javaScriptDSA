dcf7000ce8701b7f4c14cdad8d9ed8e1f67198e0
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-02-14 19:38:06 +0530

    Added some comments to SortStringArrays.js

--- sortStringArrays.js
+++ sortStringArrays.js
@@ -7,8 +7,8 @@ function sortStringArrays(arr) {
     for (let i = 0; i < arr.length-1; i++) {
         for (let j = 0; j < arr.length-i-1; j++) {
             if (arr[j]>arr[j+1]) {
-                let temp = arr[j];
-                arr[j] = arr[j+1];
+                let temp = arr[j];//Stores the value n temporarily variable
+                arr[j] = arr[j+1];//Swaps the value
                 arr[j+1] = temp;
             }
         }
